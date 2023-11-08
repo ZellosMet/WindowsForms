d8d1a29df9995f29f48aaa451ff299d957b09fc1
Zello <Zello@DESKTOP-IBMII35>
2023-11-08 23:28:32 +0900

    Add project files.

---
+++ WindowsForms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			label1.Text = DateTime.Now.ToString("HH:mm:ss");
+			if (cbShowDate.Checked) label1.Text += $"\n{DateTime.Now.ToString("dd.MM.yyyy")}";
+		}
+
+		private void label1_DoubleClick(object sender, EventArgs e)
+		{
+			this.FormBorderStyle = FormBorderStyle.Sizable;
+			this.TransparencyKey = Color.AliceBlue;
+			this.ShowInTaskbar = true;
+			this.cbShowDate.Visible = true;
+		}
+	}
+}
